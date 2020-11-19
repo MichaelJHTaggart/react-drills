ed7b2d886111ddaf6d282ede49d5d9763bc86dd3
Michael Taggart <mtaggarthawk@gmail.com>
2020-11-19 00:10:50 -0700

    tried to understand these as best as I can. I think I am getting there!

---
+++ app-7/src/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+    render() {
+        let list = this.props.tasks.map((element, index) => {
+            return <Todo key={index} task={element} />;
+        });
+
+        return <div>{list}</div>;
+    }
+}
